71d197bc3a7875768742e547f68d22a5fbaae57a
rizkymalm <rizkymalm@gmail.com>
2024-06-14 16:18:36 +0700

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 // import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import { thunk } from 'redux-thunk';
